5f3b272e3b8695c5f8ab7260ab8f693a0a1a40e6
JellyDOG <ingkarat114@gmail.com>
2020-12-03 17:44:34 +0700

    move

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -23,8 +23,8 @@
           @dragenter.prevent="drop_zone_enter"
           @dragleave.prevent="drop_zone_leave"
           @dragover.prevent
-          >
-          </div>
+          @drop="drop_item(index,task_index)"
+          ></div>
         </div>
 
         <div class="create-task" @click="create_task(index)">Create Task</div>
@@ -45,6 +45,7 @@ export default {
   props: {
     data: Array,
     create_task_submit: Function,
+    move_item_task:Function
   },
   methods: {
     create_task(index_column) {
@@ -57,7 +58,7 @@ export default {
       });
     },
     start_move(task_index, column_index) {
-      this.column_index = column_index;
+      this.current_column_index = column_index;
       this.current_task_index = task_index;
     },
     drop_zone_enter(event){
@@ -70,6 +71,15 @@ export default {
         event.target.style.borderStyle = "none";
         event.target.style.transition = "0.5s";
     },
+    drop_item(column_index,task_index){
+        console.log(column_index,task_index)
+        this.move_item_task(
+            this.current_column_index,
+            this.current_task_index,
+            column_index,
+            
+        )
+    }
   },
   data() {
     return {
